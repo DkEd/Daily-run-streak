3104eb30edff0b0ebaebcb41838b47c28e37f5fe
DkEd <jason-ou@live.co.uk>
2025-08-26 16:44:15 +0100

    Update statsRoutes.js

--- routes/statsRoutes.js
+++ routes/statsRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const { getAllStats } = require('../controllers/statsController');
-const refreshDataMiddleware = require('../middleware/refreshData'); // Add this line
+const refreshDataMiddleware = require('../middleware/refreshData');
 const router = express.Router();
 
 // Apply middleware to stats route
@@ -11,7 +11,7 @@ router.get('/stats', async (req, res) => {
     const data = await getAllStats();
     res.send(`<h1>Running Stats</h1><pre>${JSON.stringify(data, null, 2)}</pre><a href="/">Home</a>`);
   } catch (error) {
-    res.status(500).send(`<h1>Error</h1><p>${error.message}</p>`);
+    res.status(500).send(`<h1>Error</h1><p>${error.message}</p><a href="/">Home</a>`);
   }
 });
 
